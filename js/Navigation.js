4852f30fab95d02bd2c5d2afb4b62eeacff122ce
nisaljayasekara <j.nmjayasekara@gmail.com>
2024-12-01 16:21:59 +0530

    inizialized dashboard

---
+++ js/Navigation.js
@@ -0,0 +1,8 @@
+document.getElementById("DashboardForm").style.display = "block"; 
+
+
+document.getElementById("Dashboard-button").addEventListener("click", function() {
+    document.getElementById("DashboardForm").style.display = "block";
+  
+
+});
