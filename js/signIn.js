79abeeafbf7e9158c262eac819a62aa5e86e52f8
nisaljayasekara <j.nmjayasekara@gmail.com>
2024-12-05 21:01:41 +0530

    fixed errors

--- js/signIn.js
+++ js/signIn.js
@@ -3,17 +3,7 @@ const backBtn = document.getElementById('backToWelcomePage');
 const carousel = document.getElementById('carousel');
 const loginForm = document.getElementById('loginForm');
 
-// Navigate to Login Form
-navigateBtn.addEventListener('click', () => {
-    carousel.classList.add('swipe-left');
-    loginForm.classList.add('active');
-});
 
-// Navigate back to Carousel
-backBtn.addEventListener('click', () => {
-    carousel.classList.remove('swipe-left');
-    loginForm.classList.remove('active');
-});
 
 const loginBtn = document.getElementById('log');
 
